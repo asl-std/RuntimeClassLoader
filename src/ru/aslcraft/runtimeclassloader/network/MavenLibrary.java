58959808a0e3a1280bc2aef13ac8f78ec09d7adf
Snoop1CattZ69 <agronaut8323@gmail.com>
2022-08-09 18:27:54 +0300

    Revert "changed MavenLibrary from enum const to public class"
    This reverts commit 6bf0e72bd0aca5bcd12cdcb78444eff460d0c829.

--- src/ru/aslcraft/runtimeclassloader/network/MavenLibrary.java
+++ src/ru/aslcraft/runtimeclassloader/network/MavenLibrary.java
@@ -3,24 +3,23 @@ package ru.aslcraft.runtimeclassloader.network;
 /**
  * Some needed libraries for plugins (like a MySQL or Oracle frameworks)
  */
-public class MavenLibrary {
+public enum MavenLibrary {
 
-	public static MavenLibrary
-	HTTPCLIENT = new MavenLibrary(MavenRepository.Central, "org.apache.httpcomponents", "httpclient", "4.5.13"),
-	SQLITE_JDBC = new MavenLibrary(MavenRepository.Central, "org.xerial", "sqlite-jdbc", "3.39.2.1-SNAPSHOT"),
-	MYSQL_CONNECTOR = new MavenLibrary(MavenRepository.Central, "mysql", "mysql-connector-java", "8.0.30"),
-	GSON = new MavenLibrary(MavenRepository.Central, "com.google.code.gson", "gson", "2.9.0"),
-	HIKARICP = new MavenLibrary(MavenRepository.Central, "com.zaxxer", "HikariCP", "4.0.3"),
-	JDA = new MavenLibrary(MavenRepository.Central, "net.dv8tion", "JDA", "5.0.0-alpha.17"),
-	ORG_JSON = new MavenLibrary(MavenRepository.Central, "org.json", "json", "20220320"),
-	ASM = new MavenLibrary(MavenRepository.Central, "org.ow2.asm", "asm", "9.3");
+	HTTPCLIENT(MavenRepository.Central, "org.apache.httpcomponents", "httpclient", "4.5.13"),
+	SQLITE_JDBC(MavenRepository.Central, "org.xerial", "sqlite-jdbc", "3.39.2.1-SNAPSHOT"),
+	MYSQL_CONNECTOR(MavenRepository.Central, "mysql", "mysql-connector-java", "8.0.30"),
+	GSON(MavenRepository.Central, "com.google.code.gson", "gson", "2.9.0"),
+	HIKARICP(MavenRepository.Central, "com.zaxxer", "HikariCP", "4.0.3"),
+	JDA(MavenRepository.Central, "net.dv8tion", "JDA", "5.0.0-alpha.17"),
+	ORG_JSON(MavenRepository.Central, "org.json", "json", "20220320"),
+	ASM(MavenRepository.Central, "org.ow2.asm", "asm", "9.3");
 
 	private final MavenRepository repository;
 	private final String groupId;
 	private final String artifactId;
 	private final String version;
 
-	public MavenLibrary(MavenRepository repository, String groupId, String artifactId, String version) {
+	MavenLibrary(MavenRepository repository, String groupId, String artifactId, String version) {
 		this.repository = repository;
 		this.groupId = groupId;
 		this.artifactId = artifactId;
